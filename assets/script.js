a669921615a92ae9267a02f71c8a276b84e212a4
hotsoup42 <jose22gw@gmail.com>
2021-06-09 14:33:46 -0700

    formated correction

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,16 @@
 var divContainer = $(".container");
 var saveBtn = document.querySelectorAll("button");
-var hourList = ['9am', '10am', '11am', '12am', '1pm', '2pm', '3pm', '4pm','5pm',];
+var hourList = [
+  "9am",
+  "10am",
+  "11am",
+  "12am",
+  "1pm",
+  "2pm",
+  "3pm",
+  "4pm",
+  "5pm",
+];
 var timeId = ["9", "10", "11", "12", "13", "14", "15", "16", "17"];
 //id current time
 var currentDay = $("#currentDay");
@@ -8,47 +18,37 @@ var currentDay = $("#currentDay");
 //luxon time libary
 const DateTime = luxon.DateTime;
 //luxon var's
-var now  = DateTime.now().toLocaleString({ weekday: 'short', month: 'short', day: '2-digit', hour: '2-digit', minute: '2-digit' }); //=> 'Thu, Apr 20, 11:27 AM'
-
-
+var now = DateTime.now().toLocaleString({
+  weekday: "short",
+  month: "short",
+  day: "2-digit",
+  hour: "2-digit",
+  minute: "2-digit",
+}); //=> 'Thu, Apr 20, 11:27 AM'
 
 //display current time
-function showCurrentTime(){
-    currentDay.append(now);
-};
-
-
+function showCurrentTime() {
+  currentDay.append(now);
+}git
 
 //for loop for hourlist
-for (var i =0; i<hourList.length; i++){
-    //creates row by cre
-    var createRow = $("<div class='row time-block'>").attr("id", timeId[i]);
-   
- divContainer.append(createRow);
-
-    var createTime = $("<div class='hour col-1'>");
-
-    createTime.text(hourList[i]);
-
-    createRow.append(createTime);
-
+for (var i = 0; i < hourList.length; i++) {
+  //creates row by calling for a DOM eleemnt with $jquery, added class=row , timeblock.  creating the attribute of a id, assigning var timeId to the id, then looping thru var timeID with var I (loop.length)
+  var createRow = $("<div class='row time-block'>").attr("id", timeId[i]);
+  //calling .container appending createRow.
+  divContainer.append(createRow);
+  //DOM with jquery creating class=hour col-l
+  var createTime = $("<div class='hour col-1'>");
+  //display text from hour list in a loop= hourlist in var createTime
+  createTime.text(hourList[i]);
+  //append create row into createTime
+  createRow.append(createTime);
 }
 
-
-
-
-
- // Append the columns to the parent row
-
-
-
+// Append the columns to the parent row
 
 //run current time function
 showCurrentTime();
-
-
-
-
 
 // GIVEN I am using a daily planner to create a schedule
 // WHEN I open the planner
